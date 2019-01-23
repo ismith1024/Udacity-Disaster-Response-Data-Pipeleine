e0378e13e35a8528c6f48f8805d0475253aef5a5
ismith1024 <ismith1024@gmail.com>
2019-01-22 20:33:51 -0500

    feat: Implement ETL pipeline as script

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,57 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
-
+'''
+load_data
+docstring goes here
+'''
 def load_data(messages_filepath, categories_filepath):
-    pass
+    #load datasets
+    messages = pd.read_csv(messages_filepath, dtype = str)
+    categories = pd.read_csv(categories_filepath, dtype = str)
+    
+    #merge datasets
+    df = messages.set_index('id').join(categories.set_index('id'))
+    
+    return df
 
-
+'''
+clean_data
+'''
 def clean_data(df):
-    pass
+    
+    #clean up the category names 
+    categories = df['categories'].str.split(";", expand=True )
+    #split off the last two characters of the values
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda x: x[:-2])
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
 
+    #convert category data to numberic 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1:])
+        
+        # convert column from string to numeric
+        categories[column] = categories[column].apply(lambda x: int(x))
+
+    # drop the original categories column from `df`
+    df = df.drop('categories', axis=1)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df =  pd.concat([df, categories], axis=1
+    
+    # drop duplicates
+    df.drop_duplicates(keep='first', inplace=True)
+
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('InsertTableName', engine, index=False)
 
 
 def main():
