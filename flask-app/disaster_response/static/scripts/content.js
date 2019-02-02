6414d8f207f4e7f140c0f126eec2685070f28934
ismith1024 <ismith1024@gmail.com>
2019-02-02 18:43:06 -0500

    task: App in progress

--- flask-app/disaster_response/static/scripts/content.js
+++ flask-app/disaster_response/static/scripts/content.js
@@ -1,3 +1,15 @@
 var test = function(){
     console.log("This is the test function");
 }
+
+
+var get_and_replace_classification = function(message_text){
+    
+    $.post(postURL, mydata, whenItsDone);
+    
+    var new_div = '<div id="classification_results_container">' + 
+        '_____' +
+        '</div>';
+
+    $('#classification_results_container').replaceWith(new_div);
+};
