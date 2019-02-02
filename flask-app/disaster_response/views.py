7fa3a39fafff02eda4408b4f6207cdbaf2a7dbfe
ismith1024 <ismith1024@gmail.com>
2019-02-02 15:53:19 -0500

    feat: Add full flask app from template

---
+++ flask-app/disaster_response/views.py
@@ -0,0 +1,50 @@
+"""
+Routes and views for the flask application.
+"""
+
+### Flask Application Imports
+from datetime import datetime
+from flask import render_template
+from disaster_response import app
+
+### Data Science Imports
+import pandas as pd
+
+
+'''
+SERVER SETUP
+
+'''
+
+
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Disaster Response',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
