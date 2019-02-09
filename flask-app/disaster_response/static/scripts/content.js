bcd3845f28434df4da75272461a7fa21ba4348f0
ismith1024 <ismith1024@gmail.com>
2019-02-09 18:59:24 -0500

    feat: Plots on web app, REST API placeholder

--- flask-app/disaster_response/static/scripts/content.js
+++ flask-app/disaster_response/static/scripts/content.js
@@ -1,15 +1,230 @@
-var test = function(){
-    console.log("This is the test function");
-}
+
+///// DOM Element Values
+var message_types = [] //holds the messages and their types
 
 
-var get_and_replace_classification = function(message_text){
-    
-    $.post(postURL, mydata, whenItsDone);
-    
-    var new_div = '<div id="classification_results_container">' + 
-        '_____' +
-        '</div>';
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////DOM Functions
+/////////////////////////////////////////////////////////////////////////////////////////////
 
-    $('#classification_results_container').replaceWith(new_div);
+
+var updateHighlightedContract = function(){
+	var mile = $("#milesOptions").val();
+	
+	for(var i = 0; i < miles_JSON.length; i++){
+        if (miles_JSON[i].milestone == mile) {
+            selected_client = miles_JSON[i].client.trim();
+            selected_productDesc = miles_JSON[i].productDesc.trim();
+            selected_milestone = miles_JSON[i].milestone.trim();
+
+			$("#clientNameText").replaceWith('<p id="clientNameText"><b>Client:</b> ' + miles_JSON[i].client + '</p>');
+			$("#productNameText").replaceWith('<p id="productNameText"><b>Product:</b> ' + miles_JSON[i].productDesc + '</p>');
+			$("#milestoneText").replaceWith('<p id="milestoneText"><b>Milestone:</b> ' + miles_JSON[i].milestone + '</p>');			
+		}
+	}
+	
 };
+
+
+
+var resetWarning = function () {
+    $('#warning_container').replaceWith('<div id="warning_container"></div >');
+};
+
+
+//////////////////// CLASSIFIER IS HERE
+
+/*
+    update_classes(classifier_results)  
+*/
+var update_classes = function(classifier_json){
+    console.log("Updating classes:");
+    console.log(classifier_json);
+
+    var new_container_html = '<div id="classification_results_container">';
+    
+    for(i = 0; i < classifier_json.length; i++){
+        new_container_html += '<p>Results:'
+        new_container_html += classifier_json[i].msg_class + ':' + classifier_json[i].prob;
+        console.log(classifier_json[i].msg_class + ':' + classifier_json[i].prob);
+    }    
+    
+    new_container_html += '</p></div>';
+
+    $("#classification_results_container").replaceWith(new_container_html);
+
+    
+
+
+};
+
+
+/*
+classify_message()
+    - gets the message from the message box element #message_box
+    - POSTs a classify request to the server
+    - Callback: updates the message classes and probabilities
+*/
+var classify_message = function() {
+
+    var msg_string;
+    if ($("#message_box").val() != null) {
+        msg_string = $("#message_box").val(); //.replace(/ /g, "");
+    } else msg_string = "";
+
+    var classifierResults = []
+    var classifierURL = "http://localhost:5555/classifier?msg_text=" + encodeURIComponent(msg_string);
+    console.log(classifierURL);
+
+    $.getJSON(classifierURL, classifierResults, function (classifierResults) {
+        for (i = 0; i < classifierResults.length; i++) {
+            console.log(classifierResults[i].msg_class + classifierResults[i].prob);
+        }
+        
+        update_classes(classifierResults);
+
+    });
+
+    return false;
+    //resetWarning();
+
+};
+
+/*
+update_plots()
+gets the current and training set genre and class counts
+inserts the plots into the DOM
+*/
+var update_plots = function(){
+
+    var msg_results = [];
+
+    //refresh empty plot containers
+    $("#messages_by_genre_all").replaceWith('<div id="messages_by_genre_all"></div>');
+    $("#messages_by_class_all").replaceWith('<div id="messages_by_class_all"></div>');
+    $("#messages_by_genre_curr").replaceWith('<div id="messages_by_genre_curr"></div>');
+    $("#messages_by_class_curr").replaceWith('<div id="messages_by_class_curr"></div>');
+
+    //update training set messages by genre plot
+    //get the JSON data from teh server
+    //callback renders a new plot in the DOM container
+    $.getJSON("http://localhost:5555/msg_by_genre", msg_results, function (msg_results) {
+        
+        var data = [
+            {
+              x: [],
+              y: [],
+              type: 'bar'
+            }
+        ];
+
+        for(i = 0; i < msg_results.length; ++i){
+            data[0].x.push(msg_results[i].genre);
+            data[0].y.push(msg_results[i].count);
+        };
+        
+        Plotly.newPlot('messages_by_genre_all', data);
+
+    });
+
+    //update training set messages by genre plot
+    //get the JSON data from teh server
+    //callback renders a new plot in the DOM container
+    $.getJSON("http://localhost:5555/msg_by_class", msg_results, function (msg_results) {
+        
+        var data = [
+            {
+              x: [],
+              y: [],
+              type: 'bar'
+            }
+        ];
+
+        console.log('All messages by class:')
+        for(i = 0; i < msg_results.length; ++i){
+            console.log(data[0]);
+            data[0].x.push(msg_results[i].class);
+            data[0].y.push(msg_results[i].count);
+        };
+        
+        Plotly.newPlot('messages_by_class_all', data);
+
+    });
+
+
+    //update current messages by genre plot
+    //get the JSON data from teh server
+    //callback renders a new plot in the DOM container
+    $.getJSON("http://localhost:5555/curr_msgs_genre", msg_results, function (msg_results) {
+        
+        var data = [
+            {
+              x: [],
+              y: [],
+              type: 'bar'
+            }
+        ];
+
+        for(i = 0; i < msg_results.length; ++i){
+            data[0].x.push(msg_results[i].genre);
+            data[0].y.push(msg_results[i].count);
+        };
+        
+        Plotly.newPlot('messages_by_genre_curr', data);
+
+    });
+
+    //update current messages by class plot
+    //get the JSON data from teh server
+    //callback renders a new plot in the DOM container
+    $.getJSON("http://localhost:5555/curr_msgs_class", msg_results, function (msg_results) {
+        
+        console.log(msg_results);
+
+        var data = [
+            {
+              x: [],
+              y: [],
+              type: 'bar'
+            }
+        ];
+
+        console.log(data);
+
+        for(i = 0; i < msg_results.length; ++i){
+            data[0].x.push(msg_results[i].class);
+            data[0].y.push(msg_results[i].count);
+        };
+        
+        Plotly.newPlot('messages_by_class_curr', data);
+
+    });
+    
+
+    return false;
+
+};
+
+
+
+/*
+ * setup_data()
+ * runs when page loads
+ * initiates all API calls to set up the DOM
+ */
+var setup_data = function(){
+    
+    update_plots();
+
+    //Add a click listener to the submit message button
+	$(document).on('click', '#classify_button', function(){
+        classify_message();
+        update_plots();
+        return false;
+	});
+
+
+};
+
+
+
--- flask-app/disaster_response/views.py
+++ flask-app/disaster_response/views.py
@@ -11,13 +11,139 @@ from disaster_response import app
 import pandas as pd
 
 
+## Flask Server
+from datetime import datetime
+from flask import request, jsonify
+import json
+from flask import Response
+from datetime import timedelta  
+from flask import Flask, make_response, request, current_app  
+from functools import update_wrapper
+
+
+## Data science 
+import numpy as np
+import pandas as pd
+import math
+import matplotlib.pyplot as plt
+
+## sqlite3
+import sqlite3
+from sqlite3 import Error
+
+## sqlite3
+import sqlite3
+from sqlite3 import Error
+
+##for files
+import os
+from flask import Flask, flash, request, redirect, url_for
+from shutil import copyfile
+
+
 '''
 SERVER SETUP
 
 '''
+print("Starting server on port 5555")
+
+#the messages that have been received and classified from the current disaster event
+curr_messages_genre = []
+msg_row = {}
+msg_row['genre'] = 'direct'
+msg_row['count'] = 3
+curr_messages_genre.append(msg_row)
+msg_row = {}
+msg_row['genre'] = 'news'
+msg_row['count'] = 12
+curr_messages_genre.append(msg_row)
+msg_row = {}
+msg_row['genre'] = 'social'
+msg_row['count'] = 9
+curr_messages_genre.append(msg_row)
+
+curr_messages_class = []
+msg_row = {}
+msg_row['class'] = 'medical_help'
+msg_row['count'] = 7
+curr_messages_class.append(msg_row)
+msg_row = {}
+msg_row['class'] = 'storm'
+msg_row['count'] = 11
+curr_messages_class.append(msg_row)
+msg_row = {}
+msg_row['class'] = 'other_aid'
+msg_row['count'] = 4
+curr_messages_class.append(msg_row)
+msg_row = {}
+msg_row['class'] = 'tools'
+msg_row['count'] = 5
+curr_messages_class.append(msg_row)
+
+all_messages_genre = []
+msg_row = {}
+msg_row['genre'] = 'direct'
+msg_row['count'] = 23
+all_messages_genre.append(msg_row)
+msg_row = {}
+msg_row['genre'] = 'news'
+msg_row['count'] = 42
+all_messages_genre.append(msg_row)
+msg_row = {}
+msg_row['genre'] = 'social'
+msg_row['count'] = 9
+all_messages_genre.append(msg_row)
+
+all_messages_class = []
+msg_row = {}
+msg_row['class'] = 'medical_help'
+msg_row['count'] = 17
+all_messages_class.append(msg_row)
+msg_row = {}
+msg_row['class'] = 'storm'
+msg_row['count'] = 51
+all_messages_class.append(msg_row)
+msg_row = {}
+msg_row['class'] = 'other_aid'
+msg_row['count'] = 24
+all_messages_class.append(msg_row)
+msg_row = {}
+msg_row['class'] = 'tools'
+msg_row['count'] = 65
+all_messages_class.append(msg_row)
+msg_row = {}
+msg_row['class'] = 'earthquake'
+msg_row['count'] = 6
+all_messages_class.append(msg_row)
+msg_row = {}
+msg_row['class'] = 'floods'
+msg_row['count'] = 33
+all_messages_class.append(msg_row)
 
 
+def classify(msg_text):
+    '''
+        classify(msg_text)
+        in: some message text
+        out: array of {class: __, value: __}
+    '''
+    return_array = []
+    return_dict = {}
+    return_dict['msg_class'] = 'some class'
+    return_dict['prob'] = 0.75
+    return_array.append(return_dict)
+    return_dict = {}
+    return_dict['msg_class'] = 'some other class'
+    return_dict['prob'] = 0.15
+    return_array.append(return_dict)
+    return_dict = {}
+    return_dict['msg_class'] = 'yet another class'
+    return_dict['prob'] = 0.1
+    return_array.append(return_dict)  
 
+    print(return_array)
+
+    return return_array
 
 @app.route('/')
 @app.route('/home')
@@ -48,3 +174,59 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+
+'''
+A simple REST API for the application
+'''
+
+'''
+    /classifier route    
+
+'''
+@app.route('/classifier', methods=['GET'])
+def classifier():
+    query_parameters = request.args
+
+    msg_text = query_parameters.get('msg_text')
+
+    resp = classify(msg_text)
+    
+    return jsonify(resp)
+
+
+'''
+    /msg_by_genre route    
+    Returns the training set by genre
+'''
+@app.route('/msg_by_genre', methods=['GET'])
+def msg_by_genre():
+    
+    return jsonify(all_messages_genre)
+
+'''
+    /msg_by_class route   
+    Returns the training set by class
+'''
+@app.route('/msg_by_class', methods=['GET'])
+def msg_by_class():
+    
+    return jsonify(all_messages_class)
+
+'''
+    /curr_msgs_genre route
+    returns counts of message genres for the current disaster event
+'''
+@app.route('/curr_msgs_genre', methods=['GET'])
+def curr_msgs_genre():
+  
+    return jsonify(curr_messages_genre)
+
+'''
+    /curr_msgs_class route
+    returns counts of message classes for the current disaster event
+'''
+@app.route('/curr_msgs_class', methods=['GET'])
+def curr_msgs_class():
+    
+    return jsonify(curr_messages_class)
